470e24896be4753366f347c0b0d46a6074c8c977
AceTadalan <acemarloutadalan08@gmail.com>
2024-09-09 12:04:54 +0800

    Add files via upload

---
+++ calculation of sum, difference, product, quotient and remainder.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+	int x=4, y=2; 
+	
+	
+	
+	int sum= x+y;
+	cout << "Sum:" << sum << endl;
+	
+	int difference= x-y; 
+	cout << "Difference:" << difference << endl;
+	
+	int product= x*y ;
+	cout << "Product:" << product << endl;
+	
+	int quotient= x/y ;
+	cout << "Quotient:" << quotient << endl;
+	
+	int remainder= x%y;
+	cout << "Remainder:" << remainder << endl;
+	
+	return 0;
+	 
+}
