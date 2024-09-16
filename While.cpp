d9e4452799ca90ee811fdbc820d28004533a699c
AceTadalan <acemarloutadalan08@gmail.com>
2024-09-16 11:30:59 +0800

    Add files via upload

---
+++ While.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int i = 0;
+	
+	while (i<=10)
+	{ cout << "hello " << i << endl;
+	i=i+1;
+	}
+	 return 0;
+
+/*	
+	int i;
+	int j = 10 ;
+	for (i = 0; i <=j; i++)
+	{	cout << "hi " << i << endl;}
+	
+	return 0;
+	*/
+
+}
