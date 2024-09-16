e2ab97d957560a97b7520ef7d3408fa1df392572
AceTadalan <acemarloutadalan08@gmail.com>
2024-09-16 11:58:59 +0800

    Add files via upload

---
+++ Challenge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+cout << "Integers less than 10 but not less than 3"<< endl; 
+cout << ""<< endl;
+
+	int i = 9;
+	do 
+	{	cout << " " << i << endl; i--;}
+	while ( i<=9 & i>=3); 
+	
+	return 0;
+}
